067a6bb5dcde38419fa61a7893b56b6ea181034c
AndrewBez <ABezuhlyy@central-insurance.com>
2020-02-02 21:39:03 -0500

    improvement 1

--- server.R
+++ server.R
@@ -13,5 +13,15 @@ shinyServer(
                         geom_point()+
                         geom_smooth(method = 'lm')
             })
+            output$descr <- renderText({
+                  cl <- as.numeric(input$cyl)
+                  tr <- as.numeric(input$am)
+                  if(cl==0) {df1 <- mtcars}
+                  else {df1 <- mtcars[mtcars$cyl==cl,]}
+                  if(tr==2) {df2 <- df1}
+                  else {df2 <- df1[df1$am==tr,]}
+                  fit <- lm(formula=mpg~wt,data=df2)
+                  paste0(c("Intercept: ","Slope: "),fit$coefficients)
+            })
       }
 )
--- ui.R
+++ ui.R
@@ -2,12 +2,16 @@ library(shiny)
 shinyUI(pageWithSidebar(
       headerPanel("MPG regression model"),
       sidebarPanel(
-            h3('Subset by attributes'),
-            selectInput("cyl",label = "Cylinder Number", choices = list("4"=4,"6"=6,"8"=8,"all"=0), selected = 0),
-            radioButtons("am",label = "Transmission Type", choices = list("Auto"=0,"Manual"=1,"Both"=2),selected = 2)
-      ),
+            h4("Documentation: 
+               This app builds the regression model on the subset of mtcars dataset that is an R build in dataset. 
+               The MpG variable is used as the outcome and the weight variable is used as the regressor.
+               The dataset can be subsetted by either number of cylinders or transmission type or both."),
+            selectInput("cyl",label = "Select Cylinder Number", choices = list("4"=4,"6"=6,"8"=8,"all"=0), selected = 0),
+            radioButtons("am",label = "Select Transmission Type", choices = list("Auto"=0,"Manual"=1,"Both"=2),selected = 2)
+            ),
       mainPanel(
             h3('Regression Model'),
-            plotOutput(outputId="gplot")
+            plotOutput(outputId="gplot"),
+            textOutput(output="descr")
       )
-))
+      ))
